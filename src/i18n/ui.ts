c966854e4e438f9c4cdf5f6f9632fd060fcaf485
javierpoduje <javier.poduje@gmail.com>
2025-07-22 21:34:32 +0200

    feat: translate

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,16 +9,37 @@ export const languages = {
 export const ui = {
   en: {
     "whoami.text": "I'm a software engineer working at",
-    chesscom: "Chess.com",
+    "whoami.title": "Hi! I'm thisguyvims",
+    "chesscom": "Chess.com",
+    "about.streamer": "Streamer and and content creator - always about coding",
+    "about.from": "Originally from Chile, currently living in Catalonia",
+    "music.text": "I used to be a musician... kinda. You can listen the stuff in which I've played going here",
+    "social.chess": "Play with me!",
+    "profile.alt": "Image of me",
+    "header.path": "~/home/thisguyvims"
   },
 
   es: {
     "whoami.text": "Soy un ingeniero de software trabajando en",
-    chesscom: "Chess.com",
+    "whoami.title": "Hola! Soy thisguyvims",
+    "chesscom": "Chess.com",
+    "about.streamer": "Streamer y creador de contenido - siempre sobre programación",
+    "about.from": "Originario de Chile, actualmente viviendo en Cataluña",
+    "music.text": "Solía ser músico... más o menos. Puedes escuchar las cosas en las que he tocado yendo aquí",
+    "social.chess": "¡Juega conmigo!",
+    "profile.alt": "Imagen de mí",
+    "header.path": "~/home/thisguyvims"
   },
 
   ca: {
     "whoami.text": "Sóc un enginyer de programari treballant a",
-    chesscom: "Chess.com",
+    "whoami.title": "Hola! Sóc thisguyvims",
+    "chesscom": "Chess.com",
+    "about.streamer": "Streamer i creador de contingut - sempre sobre programació",
+    "about.from": "Originari de Xile, actualment vivint a Catalunya",
+    "music.text": "Solia ser músic... si fa no fa. Pots escoltar les coses en què he tocat anant aquí",
+    "social.chess": "Juga amb mi!",
+    "profile.alt": "Imatge de mi",
+    "header.path": "~/home/thisguyvims"
   },
 } as const;
