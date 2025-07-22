fe0dfa30db9b73269802d12c060408778d9d8ee2
javierpoduje <javier.poduje@gmail.com>
2025-07-22 20:43:35 +0200

    feat: add languages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import icon from "astro-icon";
 
 export default defineConfig({
   integrations: [icon()],
+  i18n: {
+    locales: ["es", "en", "ca"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,24 @@
+export const defaultLang = "en";
+
+export const languages = {
+  en: "English",
+  es: "Spanish",
+  ca: "Catalan",
+};
+
+export const ui = {
+  en: {
+    whoami: "I'm a software engineer working at",
+    chesscom: "Chess.com",
+  },
+
+  es: {
+    whoami: "Soy un ingeniero de software trabajando en",
+    chesscom: "Chess.com",
+  },
+
+  ca: {
+    whoami: "Sóc un enginyer de programari treballant a",
+    chesscom: "Chess.com",
+  },
+} as const;
